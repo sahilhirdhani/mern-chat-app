2690d4b9c4c199d2c384ec4413471b92ee6f5a3c
sahil.hirdhani <sahilhirdhani@gmail.com>
2024-04-21 17:11:45 +0530

    socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5000", {
+			const socket = io("https://mern-chat-app-jkh3.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
